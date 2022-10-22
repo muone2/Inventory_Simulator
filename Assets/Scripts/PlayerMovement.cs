8a3ff0754b6bf8404f8c950cfcaa43fac732d77d
muone2 <ifp1108@gmail.com>
2022-10-22 11:20:30 +0900

    start

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] float Vertical = 0.0f;
+    [SerializeField] float Horizontal = 0.0f;
+
+
+    private void Walk()
+    {
+
+
+
+
+    }
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
